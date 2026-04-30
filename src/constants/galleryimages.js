a8dcbb6f81ea247f0519843d276b6324f5faed48
gersonhiggins <gahg.10gerson@gmail.com>
2026-04-30 17:32:26 -0400

    fixes

--- src/constants/galleryimages.js
+++ src/constants/galleryimages.js
@@ -1,237 +1,63 @@
-import gl1_50w from '../assets/responsive/gl1-50w.webp'
-import gl1_75w from '../assets/responsive/gl1-75w.webp'
-import gl1_100w from '../assets/responsive/gl1-100w.webp'
-import gl2_50w from '../assets/responsive/gl2-50w.webp'
-import gl2_75w from '../assets/responsive/gl2-75w.webp'
-import gl2_100w from '../assets/responsive/gl2-100w.webp'
-import gl3_50w from '../assets/responsive/gl3-50w.webp'
-import gl3_75w from '../assets/responsive/gl3-75w.webp'
-import gl3_100w from '../assets/responsive/gl3-100w.webp'
-import gl4_50w from '../assets/responsive/gl4-50w.webp'
-import gl4_75w from '../assets/responsive/gl4-75w.webp'
-import gl4_100w from '../assets/responsive/gl4-100w.webp'
-import gl5_50w from '../assets/responsive/gl5-50w.webp'
-import gl5_75w from '../assets/responsive/gl5-75w.webp'
-import gl5_100w from '../assets/responsive/gl5-100w.webp'
-import gl6_50w from '../assets/responsive/gl6-50w.webp'
-import gl6_75w from '../assets/responsive/gl6-75w.webp'
-import gl6_100w from '../assets/responsive/gl6-100w.webp'
-import gl7_50w from '../assets/responsive/gl7-50w.webp'
-import gl7_75w from '../assets/responsive/gl7-75w.webp'
-import gl7_100w from '../assets/responsive/gl7-100w.webp'
-import gl8_50w from '../assets/responsive/gl8-50w.webp'
-import gl8_75w from '../assets/responsive/gl8-75w.webp'
-import gl8_100w from '../assets/responsive/gl8-100w.webp'
-import gl9_50w from '../assets/responsive/gl9-50w.webp'
-import gl9_75w from '../assets/responsive/gl9-75w.webp'
-import gl9_100w from '../assets/responsive/gl9-100w.webp'
-import gl10_50w from '../assets/responsive/gl10-50w.webp'
-import gl10_75w from '../assets/responsive/gl10-75w.webp'
-import gl10_100w from '../assets/responsive/gl10-100w.webp'
-import gl11_50w from '../assets/responsive/gl11-50w.webp'
-import gl11_75w from '../assets/responsive/gl11-75w.webp'
-import gl11_100w from '../assets/responsive/gl11-100w.webp'
-import gl12_50w from '../assets/responsive/gl12-50w.webp'
-import gl12_75w from '../assets/responsive/gl12-75w.webp'
-import gl12_100w from '../assets/responsive/gl12-100w.webp'
-import gl13_50w from '../assets/responsive/gl13-50w.webp'
-import gl13_75w from '../assets/responsive/gl13-75w.webp'
-import gl13_100w from '../assets/responsive/gl13-100w.webp'
-import gl14_50w from '../assets/responsive/gl14-50w.webp'
-import gl14_75w from '../assets/responsive/gl14-75w.webp'
-import gl14_100w from '../assets/responsive/gl14-100w.webp'
-import gl15_50w from '../assets/responsive/gl15-50w.webp'
-import gl15_75w from '../assets/responsive/gl15-75w.webp'
-import gl15_100w from '../assets/responsive/gl15-100w.webp'
-import gl16_50w from '../assets/responsive/gl16-50w.webp'
-import gl16_75w from '../assets/responsive/gl16-75w.webp'
-import gl16_100w from '../assets/responsive/gl16-100w.webp'
-import gl17_50w from '../assets/responsive/gl17-50w.webp'
-import gl17_75w from '../assets/responsive/gl17-75w.webp'
-import gl17_100w from '../assets/responsive/gl17-100w.webp'
-import gl18_50w from '../assets/responsive/gl18-50w.webp'
-import gl18_75w from '../assets/responsive/gl18-75w.webp'
-import gl18_100w from '../assets/responsive/gl18-100w.webp'
-import gl19_50w from '../assets/responsive/gl19-50w.webp'
-import gl19_75w from '../assets/responsive/gl19-75w.webp'
-import gl19_100w from '../assets/responsive/gl19-100w.webp'
-import gl20_50w from '../assets/responsive/gl20-50w.webp'
-import gl20_75w from '../assets/responsive/gl20-75w.webp'
-import gl20_100w from '../assets/responsive/gl20-100w.webp'
-import gl21_50w from '../assets/responsive/gl21-50w.webp'
-import gl21_75w from '../assets/responsive/gl21-75w.webp'
-import gl21_100w from '../assets/responsive/gl21-100w.webp'
-import gl22_50w from '../assets/responsive/gl22-50w.webp'
-import gl22_75w from '../assets/responsive/gl22-75w.webp'
-import gl22_100w from '../assets/responsive/gl22-100w.webp'
-import gl23_50w from '../assets/responsive/gl23-50w.webp'
-import gl23_75w from '../assets/responsive/gl23-75w.webp'
-import gl23_100w from '../assets/responsive/gl23-100w.webp'
-import gl24_50w from '../assets/responsive/gl24-50w.webp'
-import gl24_75w from '../assets/responsive/gl24-75w.webp'
-import gl24_100w from '../assets/responsive/gl24-100w.webp'
-import gl25_50w from '../assets/responsive/gl25-50w.webp'
-import gl25_75w from '../assets/responsive/gl25-75w.webp'
-import gl25_100w from '../assets/responsive/gl25-100w.webp'
-import gl26_50w from '../assets/responsive/gl26-50w.webp'
-import gl26_75w from '../assets/responsive/gl26-75w.webp'
-import gl26_100w from '../assets/responsive/gl26-100w.webp'
-import gl27_50w from '../assets/responsive/gl27-50w.webp'
-import gl27_75w from '../assets/responsive/gl27-75w.webp'
-import gl27_100w from '../assets/responsive/gl27-100w.webp'
-import gl28_50w from '../assets/responsive/gl28-50w.webp'
-import gl28_75w from '../assets/responsive/gl28-75w.webp'
-import gl28_100w from '../assets/responsive/gl28-100w.webp'
-import gl29_50w from '../assets/responsive/gl29-50w.webp'
-import gl29_75w from '../assets/responsive/gl29-75w.webp'
-import gl29_100w from '../assets/responsive/gl29-100w.webp'
+import gl1 from '../assets/gallery_img/gl1.webp'
+import gl2 from '../assets/gallery_img/gl2.webp'
+import gl3 from '../assets/gallery_img/gl3.webp'
+import gl4 from '../assets/gallery_img/gl4.webp'
+import gl5 from '../assets/gallery_img/gl5.webp'
+import gl6 from '../assets/gallery_img/gl6.webp'
+import gl7 from '../assets/gallery_img/gl7.webp'
+import gl8 from '../assets/gallery_img/gl8.webp'
+import gl9 from '../assets/gallery_img/gl9.webp'
+import gl10 from '../assets/gallery_img/gl10.webp'
+import gl11 from '../assets/gallery_img/gl11.webp'
+import gl12 from '../assets/gallery_img/gl12.webp'
+import gl13 from '../assets/gallery_img/gl13.webp'
+import gl14 from '../assets/gallery_img/gl14.webp'
+import gl15 from '../assets/gallery_img/gl15.webp'
+import gl16 from '../assets/gallery_img/gl16.webp'
+import gl17 from '../assets/gallery_img/gl17.webp'
+import gl18 from '../assets/gallery_img/gl18.webp'
+import gl19 from '../assets/gallery_img/gl19.webp'
+import gl20 from '../assets/gallery_img/gl20.webp'
+import gl21 from '../assets/gallery_img/gl21.webp'
+import gl22 from '../assets/gallery_img/gl22.webp'
+import gl23 from '../assets/gallery_img/gl23.webp'
+import gl24 from '../assets/gallery_img/gl24.webp'
+import gl25 from '../assets/gallery_img/gl25.webp'
+import gl26 from '../assets/gallery_img/gl26.webp'
+import gl27 from '../assets/gallery_img/gl27.webp'
+import gl28 from '../assets/gallery_img/gl28.webp'
+import gl29 from '../assets/gallery_img/gl29.webp'
 
 const gallery_img = [
-  {
-    src: gl1_100w,
-    srcset: `${gl1_50w} 50w, ${gl1_75w} 75w, ${gl1_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl2_100w,
-    srcset: `${gl2_50w} 50w, ${gl2_75w} 75w, ${gl2_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl3_100w,
-    srcset: `${gl3_50w} 50w, ${gl3_75w} 75w, ${gl3_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl4_100w,
-    srcset: `${gl4_50w} 50w, ${gl4_75w} 75w, ${gl4_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl5_100w,
-    srcset: `${gl5_50w} 50w, ${gl5_75w} 75w, ${gl5_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl6_100w,
-    srcset: `${gl6_50w} 50w, ${gl6_75w} 75w, ${gl6_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl7_100w,
-    srcset: `${gl7_50w} 50w, ${gl7_75w} 75w, ${gl7_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl8_100w,
-    srcset: `${gl8_50w} 50w, ${gl8_75w} 75w, ${gl8_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl9_100w,
-    srcset: `${gl9_50w} 50w, ${gl9_75w} 75w, ${gl9_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl10_100w,
-    srcset: `${gl10_50w} 50w, ${gl10_75w} 75w, ${gl10_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl11_100w,
-    srcset: `${gl11_50w} 50w, ${gl11_75w} 75w, ${gl11_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl12_100w,
-    srcset: `${gl12_50w} 50w, ${gl12_75w} 75w, ${gl12_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl13_100w,
-    srcset: `${gl13_50w} 50w, ${gl13_75w} 75w, ${gl13_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl14_100w,
-    srcset: `${gl14_50w} 50w, ${gl14_75w} 75w, ${gl14_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl15_100w,
-    srcset: `${gl15_50w} 50w, ${gl15_75w} 75w, ${gl15_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl16_100w,
-    srcset: `${gl16_50w} 50w, ${gl16_75w} 75w, ${gl16_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl17_100w,
-    srcset: `${gl17_50w} 50w, ${gl17_75w} 75w, ${gl17_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl18_100w,
-    srcset: `${gl18_50w} 50w, ${gl18_75w} 75w, ${gl18_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl19_100w,
-    srcset: `${gl19_50w} 50w, ${gl19_75w} 75w, ${gl19_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl20_100w,
-    srcset: `${gl20_50w} 50w, ${gl20_75w} 75w, ${gl20_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl21_100w,
-    srcset: `${gl21_50w} 50w, ${gl21_75w} 75w, ${gl21_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl22_100w,
-    srcset: `${gl22_50w} 50w, ${gl22_75w} 75w, ${gl22_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl23_100w,
-    srcset: `${gl23_50w} 50w, ${gl23_75w} 75w, ${gl23_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl24_100w,
-    srcset: `${gl24_50w} 50w, ${gl24_75w} 75w, ${gl24_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl25_100w,
-    srcset: `${gl25_50w} 50w, ${gl25_75w} 75w, ${gl25_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl26_100w,
-    srcset: `${gl26_50w} 50w, ${gl26_75w} 75w, ${gl26_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl27_100w,
-    srcset: `${gl27_50w} 50w, ${gl27_75w} 75w, ${gl27_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl28_100w,
-    srcset: `${gl28_50w} 50w, ${gl28_75w} 75w, ${gl28_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  },
-  {
-    src: gl29_100w,
-    srcset: `${gl29_50w} 50w, ${gl29_75w} 75w, ${gl29_100w} 100w`,
-    sizes: '(max-width: 500px) 50vw, (max-width: 700px) 75vw, 100vw'
-  }
+  { src: gl1, srcset: `${gl1} 1x`, sizes: '100vw' },
+  { src: gl2, srcset: `${gl2} 1x`, sizes: '100vw' },
+  { src: gl3, srcset: `${gl3} 1x`, sizes: '100vw' },
+  { src: gl4, srcset: `${gl4} 1x`, sizes: '100vw' },
+  { src: gl5, srcset: `${gl5} 1x`, sizes: '100vw' },
+  { src: gl6, srcset: `${gl6} 1x`, sizes: '100vw' },
+  { src: gl7, srcset: `${gl7} 1x`, sizes: '100vw' },
+  { src: gl8, srcset: `${gl8} 1x`, sizes: '100vw' },
+  { src: gl9, srcset: `${gl9} 1x`, sizes: '100vw' },
+  { src: gl10, srcset: `${gl10} 1x`, sizes: '100vw' },
+  { src: gl11, srcset: `${gl11} 1x`, sizes: '100vw' },
+  { src: gl12, srcset: `${gl12} 1x`, sizes: '100vw' },
+  { src: gl13, srcset: `${gl13} 1x`, sizes: '100vw' },
+  { src: gl14, srcset: `${gl14} 1x`, sizes: '100vw' },
+  { src: gl15, srcset: `${gl15} 1x`, sizes: '100vw' },
+  { src: gl16, srcset: `${gl16} 1x`, sizes: '100vw' },
+  { src: gl17, srcset: `${gl17} 1x`, sizes: '100vw' },
+  { src: gl18, srcset: `${gl18} 1x`, sizes: '100vw' },
+  { src: gl19, srcset: `${gl19} 1x`, sizes: '100vw' },
+  { src: gl20, srcset: `${gl20} 1x`, sizes: '100vw' },
+  { src: gl21, srcset: `${gl21} 1x`, sizes: '100vw' },
+  { src: gl22, srcset: `${gl22} 1x`, sizes: '100vw' },
+  { src: gl23, srcset: `${gl23} 1x`, sizes: '100vw' },
+  { src: gl24, srcset: `${gl24} 1x`, sizes: '100vw' },
+  { src: gl25, srcset: `${gl25} 1x`, sizes: '100vw' },
+  { src: gl26, srcset: `${gl26} 1x`, sizes: '100vw' },
+  { src: gl27, srcset: `${gl27} 1x`, sizes: '100vw' },
+  { src: gl28, srcset: `${gl28} 1x`, sizes: '100vw' },
+  { src: gl29, srcset: `${gl29} 1x`, sizes: '100vw' }
 ]
 
 export default gallery_img
